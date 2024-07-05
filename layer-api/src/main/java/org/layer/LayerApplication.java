6081fa5a7fefd0e8f30bae7baa7a16ec8050cea9
clean2001 <clearrworld@gmail.com>
2024-07-05 14:38:25 +0900

    chore: LayerApplication.java 위치 변경

---
+++ layer-api/src/main/java/org/layer/LayerApplication.java
@@ -0,0 +1,12 @@
+package org.layer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LayerApplication.class, args);
+    }
+}
