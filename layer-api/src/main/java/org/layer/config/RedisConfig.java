34928636c4320ec22ebe0c4f60603733e5dca074
Raymond <akdfhr2@gmail.com>
2024-07-06 00:23:12 +0900

    test: 환경변수 주입 테스트

--- layer-api/src/main/java/org/layer/config/RedisConfig.java
+++ layer-api/src/main/java/org/layer/config/RedisConfig.java
@@ -4,7 +4,6 @@ import org.layer.member.Member;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -12,7 +11,6 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-//@PropertySource("classpath:application-auth.properties")
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
