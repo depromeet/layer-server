6cfdfd8055059518da7b5bc36b39072e0b2517ab
Raymond <akdfhr2@gmail.com>
2024-07-09 00:49:23 +0900

    refactor: 코멘트 수정사항 반영

---
+++ layer-api/src/main/java/org/layer/resolver/MemberIdResolver.java
@@ -0,0 +1,45 @@
+package org.layer.resolver;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.layer.common.annotation.MemberId;
+import org.layer.common.exception.BaseCustomException;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.Optional;
+
+import static org.layer.common.exception.MemberExceptionType.UNAUTHORIZED_USER;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class MemberIdResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        // 1. 어노테이션 체크
+        var annotation = parameter.hasParameterAnnotation(MemberId.class);
+
+        // 2. 파라미터의 타입 체크
+        var parameterType = parameter.getParameterType().equals(Long.class);
+
+        return (annotation && parameterType);
+    }
+
+    @Override
+    public Long resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        try{
+            var securityContext = SecurityContextHolder.getContext();
+            var memberId = Optional.ofNullable(securityContext).map(it -> it.getAuthentication().getName()).orElseThrow(() -> new BaseCustomException(UNAUTHORIZED_USER));
+
+            return Long.parseLong(memberId);
+        } catch(Exception e) {
+            throw new BaseCustomException(UNAUTHORIZED_USER);
+        }
+    }
+}
