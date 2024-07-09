4e53cefc160518185b31325af6929e53921f36cc
mikekks <thdalsrb79@naver.com>
2024-07-09 16:39:14 +0900

    chore: 여러 모듈에서 빈 조회하도록 설정

--- layer-api/src/main/java/org/layer/LayerApplication.java
+++ layer-api/src/main/java/org/layer/LayerApplication.java
@@ -3,8 +3,14 @@ package org.layer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.layer.common", "org.layer.api", "org.layer.domain", "org.layer.external"})
+@EnableJpaRepositories(basePackages = {"org.layer.domain"})
+@EntityScan(basePackages = {"org.layer.domain"})
 public class LayerApplication {
     public static void main(String[] args) {
         SpringApplication.run(LayerApplication.class, args);
