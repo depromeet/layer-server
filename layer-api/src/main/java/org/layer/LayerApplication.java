f9f4242a199a24798bd0f8269403d4e8d4d4077b
mikekks <thdalsrb79@naver.com>
2024-07-09 20:56:10 +0900

    chore: BaseTimeEntity 추가

--- layer-api/src/main/java/org/layer/LayerApplication.java
+++ layer-api/src/main/java/org/layer/LayerApplication.java
@@ -3,14 +3,10 @@ package org.layer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.layer.common", "org.layer.api", "org.layer.domain", "org.layer.external"})
-@EnableJpaRepositories(basePackages = {"org.layer.domain"})
-@EntityScan(basePackages = {"org.layer.domain"})
+@EnableJpaAuditing
 public class LayerApplication {
     public static void main(String[] args) {
         SpringApplication.run(LayerApplication.class, args);
