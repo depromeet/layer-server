fbb7cc5ee8959f6e450c671a3055299aec29194f
clean2001 <clearrworld@gmail.com>
2024-07-13 20:00:07 +0900

    feat: RetrospectTemplate 엔티티 생성

---
+++ layer-domain/src/main/java/org/layer/domain/template/entity/RetrospectTemplate.java
@@ -0,0 +1,21 @@
+package org.layer.domain.template.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.layer.domain.BaseEntity;
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class RetrospectTemplate extends BaseEntity {
+    @NotNull
+    private Long id;
+    @NotNull
+    private String title; // ex. 빠르고 효율적인 회고
+    @NotNull
+    private String templateName; // ex. KPT 회고
+    @NotNull
+    private String description; // 회고에 대한 설명
+}
