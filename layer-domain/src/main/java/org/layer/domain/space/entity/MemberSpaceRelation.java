e425aa44c13b8d1b471994ec61e188bb6625d947
mikekks <thdalsrb79@naver.com>
2024-07-13 21:49:36 +0900

    chore: Space 연관관계 추가

--- layer-domain/src/main/java/org/layer/domain/space/entity/MemberSpaceRelation.java
+++ layer-domain/src/main/java/org/layer/domain/space/entity/MemberSpaceRelation.java
@@ -1,6 +1,9 @@
 package org.layer.domain.space.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.layer.domain.BaseEntity;
@@ -14,6 +17,8 @@ public class MemberSpaceRelation extends BaseEntity {
     @NotNull
     private Long memberId;
 
-    @NotNull
-    private Long spaceId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "space_id")
+    private Space space;
+
 }
