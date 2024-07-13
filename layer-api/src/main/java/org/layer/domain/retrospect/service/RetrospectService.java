50695635567c794337099efcee5b803a80c56a38
mikekks <thdalsrb79@naver.com>
2024-07-13 21:54:09 +0900

    feat: 회고 목록 조회 서비스 로직 구현

--- layer-api/src/main/java/org/layer/domain/retrospect/service/RetrospectService.java
+++ layer-api/src/main/java/org/layer/domain/retrospect/service/RetrospectService.java
@@ -1,19 +1,33 @@
 package org.layer.domain.retrospect.service;
 
+import java.util.List;
+
+import org.layer.domain.answer.entity.Answers;
+import org.layer.domain.answer.repository.AnswerRepository;
 import org.layer.domain.retrospect.entity.Retrospect;
 import org.layer.domain.retrospect.entity.RetrospectStatus;
 import org.layer.domain.retrospect.repository.RetrospectRepository;
+import org.layer.domain.retrospect.service.dto.response.RetrospectGetServiceResponse;
+import org.layer.domain.retrospect.service.dto.response.RetrospectListGetServiceResponse;
+import org.layer.domain.space.entity.MemberSpaceRelations;
+import org.layer.domain.space.repository.MemberSpaceRelationRepository;
+import org.layer.domain.space.repository.SpaceRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class RetrospectService {
 
 	private final RetrospectRepository retrospectRepository;
+	private final MemberSpaceRelationRepository memberSpaceRelationRepository;
+	private final AnswerRepository answerRepository;
 
-	public void create(Long spaceId, Long formId, String title, String introduction){
+	@Transactional
+	public void create(Long spaceId, Long formId, String title, String introduction) {
 		Retrospect retrospect = Retrospect.builder()
 			.title(title)
 			.formId(formId)
@@ -23,5 +37,26 @@ public class RetrospectService {
 			.build();
 
 		retrospectRepository.save(retrospect);
+	}
+
+	public RetrospectListGetServiceResponse getRetrospects(Long spaceId, Long memberId) {
+
+		MemberSpaceRelations memberSpaceRelations = new MemberSpaceRelations(
+			memberSpaceRelationRepository.findAllBySpaceId(spaceId));
+
+		// 해당 멤버가 요청한 스페이스 소속 여부 확인
+		memberSpaceRelations.validateTeamMembership(memberId);
+
+		List<Retrospect> retrospects = retrospectRepository.findAllBySpaceId(spaceId);
+		List<Long> retrospectIds = retrospects.stream().map(Retrospect::getId).toList();
+		Answers answers = new Answers(answerRepository.findAllByRetrospectIdIn(retrospectIds));
+
+		List<RetrospectGetServiceResponse> retrospectDtos = retrospects.stream()
+			.map(r -> RetrospectGetServiceResponse.of(r.getTitle(), r.getIntroduction(),
+				answers.hasRetrospectAnswer(memberId), r.getRetrospectStatus(), answers.getWriteCount()))
+			.toList();
+
+		return RetrospectListGetServiceResponse.of(retrospects.size(), memberSpaceRelations.getTeamMemberCount(),
+			retrospectDtos);
 	}
 }
--- layer-domain/src/main/java/org/layer/domain/space/repository/MemberSpaceRelationRepository.java
+++ layer-domain/src/main/java/org/layer/domain/space/repository/MemberSpaceRelationRepository.java
@@ -1,7 +1,12 @@
 package org.layer.domain.space.repository;
 
+
+import java.util.List;
+
 import org.layer.domain.space.entity.MemberSpaceRelation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberSpaceRelationRepository extends JpaRepository<MemberSpaceRelation, Long> {
+
+	List<MemberSpaceRelation> findAllBySpaceId(Long spaceId);
 }
