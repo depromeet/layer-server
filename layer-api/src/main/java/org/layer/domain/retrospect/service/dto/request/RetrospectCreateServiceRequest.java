92cd969f6dc103e462d6d7fac4e89e816a0c624f
mikekks <thdalsrb79@naver.com>
2024-07-14 03:02:48 +0900

    add: dto 추가

--- layer-api/src/main/java/org/layer/domain/retrospect/service/dto/request/RetrospectCreateServiceRequest.java
+++ layer-api/src/main/java/org/layer/domain/retrospect/service/dto/request/RetrospectCreateServiceRequest.java
@@ -4,8 +4,14 @@ import java.util.List;
 
 public record RetrospectCreateServiceRequest(
 	String title,
+	String introduction,
 	Long spaceId,
 	List<String> questions,
 	boolean isMyForm
 ) {
+	public static RetrospectCreateServiceRequest of(String title, String introduction, Long spaceId, List<String> questions,
+		boolean isMyForm) {
+
+		return new RetrospectCreateServiceRequest(title, introduction, spaceId, questions, isMyForm);
+	}
 }
