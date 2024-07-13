45324a591de5d3a4a9dc681501b47cf1a5b444fc
mikekks <thdalsrb79@naver.com>
2024-07-14 00:10:03 +0900

    fix: 빌드 실패 해결

--- layer-domain/src/main/java/org/layer/domain/space/repository/SpaceRepository.java
+++ layer-domain/src/main/java/org/layer/domain/space/repository/SpaceRepository.java
@@ -1,15 +1,14 @@
 package org.layer.domain.space.repository;
 
-import org.layer.domain.space.dto.SpaceWithMemberCount;
+import static org.layer.common.exception.SpaceExceptionType.*;
+
 import org.layer.domain.space.entity.Space;
-import org.layer.domain.space.entity.SpaceCategory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.layer.domain.space.exception.SpaceException;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 
-public interface SpaceRepository extends JpaRepository<Space, Long> {
+
+public interface SpaceRepository extends JpaRepository<Space, Long>, SpaceCustomRepository {
     default Space findByIdOrThrow(Long spaceId){
         return findById(spaceId)
             .orElseThrow(() -> new SpaceException(NOT_FOUND_SPACE));
