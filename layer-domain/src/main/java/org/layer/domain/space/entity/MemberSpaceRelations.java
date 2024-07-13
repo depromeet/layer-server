8ec414f9a12e04146c80c5e849369c4344bd8da0
mikekks <thdalsrb79@naver.com>
2024-07-13 21:51:26 +0900

    feat: 1. 팀 관련 로직을 위한 일급컬렉션 생성 2. 스페이스 내 회원인지 검증 로직 추가 3. 팀 멤버 수 계산 로직 추가

---
+++ layer-domain/src/main/java/org/layer/domain/space/entity/MemberSpaceRelations.java
@@ -0,0 +1,25 @@
+package org.layer.domain.space.entity;
+
+import static org.layer.common.exception.MemberSpaceRelationExceptionType.*;
+
+import java.util.List;
+
+import org.layer.domain.space.exception.MemberSpaceRelationException;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class MemberSpaceRelations {
+	private final List<MemberSpaceRelation> memberSpaceRelations;
+
+	public void validateTeamMembership(Long memberId){
+		memberSpaceRelations.stream()
+			.filter(memberSpaceRelation -> memberSpaceRelation.getMemberId().equals(memberId))
+			.findAny()
+			.orElseThrow(() -> new MemberSpaceRelationException(NOT_FOUND_MEMBER_SPACE_RELATION));
+	}
+
+	public int getTeamMemberCount(){
+		return memberSpaceRelations.size();
+	}
+}
