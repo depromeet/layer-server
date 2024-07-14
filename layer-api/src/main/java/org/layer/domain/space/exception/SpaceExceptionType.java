222c27ac88fb51328fd7921c8ff166aab079a106
Raymond <akdfhr2@gmail.com>
2024-07-14 16:07:23 +0900

    feat: 스페이스 초대하기/떠나기 API

--- layer-api/src/main/java/org/layer/domain/space/exception/SpaceExceptionType.java
+++ layer-api/src/main/java/org/layer/domain/space/exception/SpaceExceptionType.java
@@ -9,11 +9,13 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 @RequiredArgsConstructor
 public enum SpaceExceptionType implements ExceptionType {
 
-    /**
-     *
-     */
 
-    SPACE_NOT_FOUND(BAD_REQUEST, "스페이스를 찾을 수 없어요.");
+    SPACE_NOT_FOUND(BAD_REQUEST, "스페이스를 찾을 수 없어요."),
+
+    SPACE_NOT_JOINED(BAD_REQUEST, "참여중인 스페이스가 아니에요."),
+    SPACE_LEADER_CANNOT_LEAVE(BAD_REQUEST, "스페이스를 팀장은 떠날 수 없어요."),
+    SPACE_ALREADY_JOINED(BAD_REQUEST, "이미 가입한 스페이스에요.");
+
 
     private final HttpStatus status;
     private final String message;
