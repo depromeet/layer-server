0a31fa4f179b06b2ea1ba26f62cec2d9c4da43f5
clean2001 <clearrworld@gmail.com>
2024-07-14 21:25:52 +0900

    chore: @PreAuthorize 추가

--- layer-api/src/main/java/org/layer/domain/template/controller/TemplateController.java
+++ layer-api/src/main/java/org/layer/domain/template/controller/TemplateController.java
@@ -6,6 +6,7 @@ import org.layer.domain.template.dto.*;
 import org.layer.domain.template.service.TemplateService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ public class TemplateController implements TemplateApi {
     //== 간단 정보 단건 조회 ==//
     @Override
     @GetMapping("/{templateId}/simple-info")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<TemplateSimpleInfoResponse> getTemplateSimpleInfo(@PathVariable("templateId") Long templateId) {
         TemplateSimpleInfoResponse templateSimpleInfo = templateService.getTemplateSimpleInfo(templateId);
         return new ResponseEntity<>(templateSimpleInfo, HttpStatus.OK);
@@ -28,6 +30,7 @@ public class TemplateController implements TemplateApi {
     //== 상세 정보 단건 조회 ==//
     @Override
     @GetMapping("/{templateId}/detail-info")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<TemplateDetailInfoResponse> getTemplateDetailInfo(@PathVariable("templateId") Long templateId) {
         TemplateDetailInfoResponse templateDetailInfo = templateService.getTemplateDetailInfo(templateId);
         return new ResponseEntity<>(templateDetailInfo, HttpStatus.OK);
@@ -36,6 +39,7 @@ public class TemplateController implements TemplateApi {
     //== 질문을 포함한 간단 정보 ==//
     @Override
     @GetMapping("/{templateId}/question-list")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<TemplateQuestionListResponse> getTemplateQuestionList(@PathVariable("templateId") Long templateId) {
         TemplateQuestionListResponse templateQuestions = templateService.getTemplateQuestions(templateId);
         return new ResponseEntity<>(templateQuestions, HttpStatus.OK);
@@ -45,6 +49,7 @@ public class TemplateController implements TemplateApi {
     //== 모든 템플릿 ==//
     @Override
     @GetMapping("/all")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<TemplateListResponse> getAllTemplates(AllTemplatesRequest allTemplatesRequest) {
         return new ResponseEntity<>(templateService.getAllTemplates(allTemplatesRequest.page(), allTemplatesRequest.size()), HttpStatus.OK);
     }
