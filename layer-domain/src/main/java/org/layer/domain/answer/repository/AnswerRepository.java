4eff17226df35ea962cc92ca83fae7fe43ec3156
mikekks <thdalsrb79@naver.com>
2024-07-14 22:37:25 +0900

    feat: 회고 작성 로직 구현

--- layer-domain/src/main/java/org/layer/domain/answer/repository/AnswerRepository.java
+++ layer-domain/src/main/java/org/layer/domain/answer/repository/AnswerRepository.java
@@ -1,10 +1,13 @@
 package org.layer.domain.answer.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.layer.domain.answer.entity.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 	List<Answer> findAllByRetrospectIdIn(List<Long> retrospectIds);
+
+	Optional<Answer> findByRetrospectIdAndQuestionIdAndMemberId(Long retrospectId, Long questionId, Long memberId);
 }
--- layer-domain/src/main/java/org/layer/domain/question/repository/QuestionRepository.java
+++ layer-domain/src/main/java/org/layer/domain/question/repository/QuestionRepository.java
@@ -1,8 +1,10 @@
 package org.layer.domain.question.repository;
 
+import java.util.List;
+
 import org.layer.domain.question.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-
+	List<Question> findAllByIdIn(List<Long> questionIds);
 }
