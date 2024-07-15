4ff7402de14b92f1258a028c1f5500a6deee42c2
clean2001 <clearrworld@gmail.com>
2024-07-15 18:44:33 +0900

    style: 주석 삭제

--- layer-domain/src/main/java/org/layer/domain/actionItem/repository/ActionItemRepository.java
+++ layer-domain/src/main/java/org/layer/domain/actionItem/repository/ActionItemRepository.java
@@ -4,5 +4,4 @@ import org.layer.domain.actionItem.entity.ActionItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ActionItemRepository extends JpaRepository<ActionItem, Long> {
-//    List<ActionItem> findAllByMemberIdAndActionItemStatusOrderByCreatedTimeDesc(Long memberId, ActionItemStatus actionItemStatus);
 }
