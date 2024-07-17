4f10469de4a399c30a6e9074fca955054cfca6a3
Raymond <akdfhr2@gmail.com>
2024-07-18 00:08:17 +0900

    fix: swagger 오류 수정

--- layer-api/src/main/java/org/layer/LayerApplication.java
+++ layer-api/src/main/java/org/layer/LayerApplication.java
@@ -1,10 +1,13 @@
 package org.layer;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://api.layerapp.io", description = "운영서버")})
 @SpringBootApplication
 @EnableJpaAuditing
 public class LayerApplication {
