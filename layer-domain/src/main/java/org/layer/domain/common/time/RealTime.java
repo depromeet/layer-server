52e1d602bd2ebd86000d7fc42bfd78702b977d7f
mikekks <thdalsrb79@naver.com>
2024-07-27 14:03:38 +0900

    fix: 컴포넌트 스캔 오류 해결

--- layer-domain/src/main/java/org/layer/domain/common/time/RealTime.java
+++ layer-domain/src/main/java/org/layer/domain/common/time/RealTime.java
@@ -2,6 +2,9 @@ package org.layer.domain.common.time;
 
 import java.time.LocalDateTime;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class RealTime implements Time {
 	@Override
 	public LocalDateTime now() {
