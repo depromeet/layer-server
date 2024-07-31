61b0a72e284856946d6c4ce501ebf3fd77f29db5
mikekks <thdalsrb79@naver.com>
2024-08-01 00:41:28 +0900

    feat: 폼 엔티티 수정

--- layer-domain/src/main/java/org/layer/domain/form/entity/Form.java
+++ layer-domain/src/main/java/org/layer/domain/form/entity/Form.java
@@ -1,6 +1,8 @@
 package org.layer.domain.form.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.layer.domain.BaseEntity;
@@ -9,12 +11,16 @@ import org.layer.domain.BaseEntity;
 @Getter
 @Entity
 @EqualsAndHashCode(callSuper = true)
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Form extends BaseEntity {
 
+    /*
+     * 해당 폼 생성한 멤버 id
+     */
     @NotNull
     private Long memberId;
+
+    private Long spaceId;
 
     @NotNull
     private String title;
@@ -22,4 +28,16 @@ public class Form extends BaseEntity {
     @NotNull
     private String introduction;
 
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private FormType formType;
+
+    @Builder
+    public Form(Long memberId, Long spaceId, String title, String introduction, FormType formType) {
+        this.memberId = memberId;
+        this.spaceId = spaceId;
+        this.title = title;
+        this.introduction = introduction;
+        this.formType = formType;
+    }
 }
