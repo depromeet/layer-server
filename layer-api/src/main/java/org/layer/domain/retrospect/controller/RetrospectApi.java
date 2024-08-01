356edd4badff4fc162738e6c84ec16d2d4432616
Raymond <akdfhr2@gmail.com>
2024-08-02 02:12:31 +0900

    Feat/#69 fix 스페이스 떠나기 요청방식변경 (#71)
    * fix: 스페이스 떠나기 요청방식변경
    
    * fix: 자잘한 이슈 수정

--- layer-api/src/main/java/org/layer/domain/retrospect/controller/RetrospectApi.java
+++ layer-api/src/main/java/org/layer/domain/retrospect/controller/RetrospectApi.java
@@ -1,23 +1,23 @@
 package org.layer.domain.retrospect.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.layer.common.annotation.MemberId;
 import org.layer.domain.retrospect.controller.dto.request.RetrospectCreateRequest;
+import org.layer.domain.retrospect.controller.dto.response.RetrospectCreateResponse;
 import org.layer.domain.retrospect.controller.dto.response.RetrospectListGetResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-
 @Tag(name = "회고", description = "회고 관련 API")
 public interface RetrospectApi {
 
-	@Operation(summary = "회고 생성", description = "")
-	ResponseEntity<Void> createRetrospect(@PathVariable("spaceId") Long spaceId,
-		@RequestBody @Valid RetrospectCreateRequest request, @MemberId Long memberId);
+    @Operation(summary = "회고 생성", description = "회고 생성 이후 생성된 회고의 아이디를 응답합니다.")
+    ResponseEntity<RetrospectCreateResponse> createRetrospect(@PathVariable("spaceId") Long spaceId,
+                                                              @RequestBody @Valid RetrospectCreateRequest request, @MemberId Long memberId);
 
-	@Operation(summary = "회고 목록 조회", description = "특정 팀 스페이스에서 작성했던 회고 목록을 보는 기능입니다.")
-	ResponseEntity<RetrospectListGetResponse> getRetrospects(@PathVariable("spaceId") Long spaceId, @MemberId Long memberId);
+    @Operation(summary = "회고 목록 조회", description = "특정 팀 스페이스에서 작성했던 회고 목록을 보는 기능입니다.")
+    ResponseEntity<RetrospectListGetResponse> getRetrospects(@PathVariable("spaceId") Long spaceId, @MemberId Long memberId);
 }
