12a0def7b403c21ce1026cd6d39298f8e2b14b2e
Raymond <akdfhr2@gmail.com>
2024-08-04 02:34:43 +0900

    feat: 회고 조회 응답 필드 추가하기 (#105) (#106)

--- layer-api/src/main/java/org/layer/domain/retrospect/service/dto/response/RetrospectGetServiceResponse.java
+++ layer-api/src/main/java/org/layer/domain/retrospect/service/dto/response/RetrospectGetServiceResponse.java
@@ -2,18 +2,24 @@ package org.layer.domain.retrospect.service.dto.response;
 
 import org.layer.domain.retrospect.entity.RetrospectStatus;
 
+import java.time.LocalDateTime;
+
 public record RetrospectGetServiceResponse(
-	Long retrospectId,
-	String title,
-	String introduction,
-	boolean isWrite,
-	RetrospectStatus retrospectStatus,
-	long writeCount,
-	long totalCount
+        Long retrospectId,
+        String title,
+        String introduction,
+        boolean isWrite,
+        RetrospectStatus retrospectStatus,
+        long writeCount,
+        long totalCount,
+
+        LocalDateTime createdAt,
+
+        LocalDateTime deadline
 ) {
-	public static RetrospectGetServiceResponse of(Long retrospectId, String title, String introduction, boolean isWrite,
-		RetrospectStatus retrospectStatus, long writeCount, long totalCount) {
-		return new RetrospectGetServiceResponse(retrospectId, title, introduction, isWrite, retrospectStatus,
-			writeCount, totalCount);
-	}
+    public static RetrospectGetServiceResponse of(Long retrospectId, String title, String introduction, boolean isWrite,
+                                                  RetrospectStatus retrospectStatus, long writeCount, long totalCount, LocalDateTime createdAt, LocalDateTime deadline) {
+        return new RetrospectGetServiceResponse(retrospectId, title, introduction, isWrite, retrospectStatus,
+                writeCount, totalCount, createdAt, deadline);
+    }
 }
