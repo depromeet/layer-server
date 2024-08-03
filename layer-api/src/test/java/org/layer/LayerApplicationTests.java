54949abab5fe97a82c9e07b5f334ee664f92f456
mikekks <thdalsrb79@naver.com>
2024-08-04 06:48:12 +0900

    test: 테스트코드 환경 세팅

---
+++ layer-api/src/test/java/org/layer/LayerApplicationTests.java
@@ -0,0 +1,14 @@
+package org.layer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class LayerApplicationTests {
+	@Test
+	void init(){
+
+	}
+}
