594b7c53eaea87e61c26ad2ec4b24a0d44788ade
Sejeong Kim <clearrworld@gmail.com>
2024-08-04 01:08:43 +0900

    chore: http method patch로 변경

--- layer-api/src/main/java/org/layer/domain/member/controller/MemberController.java
+++ layer-api/src/main/java/org/layer/domain/member/controller/MemberController.java
@@ -8,10 +8,7 @@ import org.layer.domain.member.controller.dto.UpdateMemberInfoResponse;
 import org.layer.domain.member.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
@@ -20,7 +17,7 @@ public class MemberController implements MemberApi {
     private final MemberService memberService;
 
     @Override
-    @PostMapping("/update-profile")
+    @PatchMapping("/update-profile")
     public ResponseEntity<UpdateMemberInfoResponse> updateMemberInfo(@MemberId Long memberId, @Valid @RequestBody UpdateMemberInfoRequest updateMemberInfoRequest) {
         UpdateMemberInfoResponse response = memberService.updateMemberInfo(memberId, updateMemberInfoRequest);
 
