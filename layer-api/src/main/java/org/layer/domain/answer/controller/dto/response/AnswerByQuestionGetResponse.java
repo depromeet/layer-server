c8f1f4b7f8bd0290db0b95fae0ca41b489a227c2
mikekks <thdalsrb79@naver.com>
2024-08-04 07:12:01 +0900

    chore: 스웨거 추가

--- layer-api/src/main/java/org/layer/domain/answer/controller/dto/response/AnswerByQuestionGetResponse.java
+++ layer-api/src/main/java/org/layer/domain/answer/controller/dto/response/AnswerByQuestionGetResponse.java
@@ -2,9 +2,15 @@ package org.layer.domain.answer.controller.dto.response;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "AnswerByQuestionGetResponse", description = "임시 회고 조회 응답 Dto")
 public record AnswerByQuestionGetResponse(
+	@Schema(description = "질문 내용", example = "질문 내용입니당")
 	String questionContent,
+	@Schema(description = "질문 타입", example = "plain_text")
 	String questionType,
+	@Schema(description = "질문 객체", example = "")
 	List<PersonAndAnswerGetResponse> answers
 ) {
 }
