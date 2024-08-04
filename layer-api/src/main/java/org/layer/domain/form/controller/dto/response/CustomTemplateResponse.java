19dd56f739bfe6e15e700bcc161c510ed7da483b
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-08-04 18:53:45 +0900

    fix: 스페이스 커스텀 폼 조회 응답에 폼아이디 추가 (#126)

--- layer-api/src/main/java/org/layer/domain/form/controller/dto/response/CustomTemplateResponse.java
+++ layer-api/src/main/java/org/layer/domain/form/controller/dto/response/CustomTemplateResponse.java
@@ -10,6 +10,9 @@ import java.time.LocalDateTime;
 @Schema(name = "DefaultFormGetResponse", description = "커스텀 템플릿 조회 간단 정보 응답 Dto")
 public record CustomTemplateResponse(
         @NotNull
+        @Schema(description = "회고폼 ID", example = "1")
+        Long id,
+        @NotNull
         @Schema(description = "회고폼 이름", example = "Mad Sad Glad 커스텀 템플릿")
         String title,
         @NotNull
