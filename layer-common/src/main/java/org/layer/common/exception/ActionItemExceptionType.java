1797aa2f430454cd74cbe5673d68d220f2383f32
Sejeong Kim <clearrworld@gmail.com>
2024-08-15 19:05:46 +0900

    style: exception messagea 수정

--- layer-common/src/main/java/org/layer/common/exception/ActionItemExceptionType.java
+++ layer-common/src/main/java/org/layer/common/exception/ActionItemExceptionType.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum ActionItemExceptionType implements ExceptionType{
     NOT_FOUND_ACTION_ITEM(HttpStatus.NOT_FOUND, "실행 목표가 존재하지 않습니다."),
     INVALID_ACTION_ITEM_ID(HttpStatus.BAD_REQUEST, "잘못된 실행 목표 id입니다."),
-    INVALID_ACTION_ITEM_LIST(HttpStatus.BAD_REQUEST, "실행 목표 리스트가 잘못 되었습니다.");
+    INVALID_ACTION_ITEM_LIST(HttpStatus.BAD_REQUEST, "잘못된 실행 목표 리스트입니다.");
 
 
     private final HttpStatus status;
