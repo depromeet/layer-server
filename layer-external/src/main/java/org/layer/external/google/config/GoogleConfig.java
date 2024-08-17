c5eaadbe8a7967f1677d18b0a2f5b039acfd603a
Raymond <akdfhr2@gmail.com>
2024-08-17 18:19:54 +0900

    fix: credentials 주입 테스트

--- layer-external/src/main/java/org/layer/external/google/config/GoogleConfig.java
+++ layer-external/src/main/java/org/layer/external/google/config/GoogleConfig.java
@@ -15,16 +15,12 @@ import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.layer.external.google.service.GoogleApiService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
@@ -33,6 +29,7 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 public class GoogleConfig {
+    private final GoogleCredentials googleCredentials;
 
     private final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
     private final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
@@ -52,16 +49,9 @@ public class GoogleConfig {
 
 
     private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-        String CREDENTIALS_FILE_PATH = "/credentials.json";
+        GoogleClientSecrets clientSecrets = createGoogleClientSecrets();
 
 
-        InputStream in = GoogleApiService.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-        }
-
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
         Resource resource = resourceLoader.getResource("classpath:tokens");
 
 
@@ -72,5 +62,18 @@ public class GoogleConfig {
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    private GoogleClientSecrets createGoogleClientSecrets() {
+        // Details 객체 생성
+        GoogleClientSecrets.Details details = new GoogleClientSecrets.Details()
+                .setClientId(googleCredentials.getInstalled().getClientId())
+                .setClientSecret(googleCredentials.getInstalled().getClientSecret())
+                .setAuthUri(googleCredentials.getInstalled().getAuthUri())
+                .setTokenUri(googleCredentials.getInstalled().getTokenUri())
+                .setRedirectUris(googleCredentials.getInstalled().getRedirectUris());
+
+        // GoogleClientSecrets 객체 생성 및 Details 설정
+        return new GoogleClientSecrets().setInstalled(details);
     }
 }
