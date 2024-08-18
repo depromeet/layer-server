cdbadf3f7926c31094df51a7e0720da2b6b870a0
mikekks <thdalsrb79@naver.com>
2024-08-18 23:28:45 +0900

    bug: 테이블 이름 변경

--- layer-domain/src/main/java/org/layer/domain/analyze/entity/Analyze.java
+++ layer-domain/src/main/java/org/layer/domain/analyze/entity/Analyze.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "analyze")
+@Table(name = "`analyze`")
 public class Analyze {
 
 	@Id
