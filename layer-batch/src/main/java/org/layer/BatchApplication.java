a4fab134e68a7b64c6c5687cf9de1cfdcf7d3f22
mikekks <thdalsrb79@naver.com>
2024-08-26 22:32:13 +0900

    feat: 회고 자동 마감 스케줄러 구현

---
+++ layer-batch/src/main/java/org/layer/BatchApplication.java
@@ -0,0 +1,13 @@
+package org.layer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class BatchApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BatchApplication.class, args);
+	}
+}
---
+++ layer-batch/src/main/java/org/layer/batch/scheduler/RetrospectScheduler.java
@@ -0,0 +1,29 @@
+package org.layer.batch.scheduler;
+
+import java.time.LocalDateTime;
+
+import org.layer.domain.common.time.Time;
+import org.layer.domain.retrospect.repository.RetrospectRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class RetrospectScheduler {
+	private final RetrospectRepository retrospectRepository;
+	private final Time time;
+
+	/**
+	 * @note: 1시간마다 실행된다.
+	 * */
+	@Scheduled(cron = "0 0 * * * *")
+	public void updateRetrospectStatusToDone() {
+		LocalDateTime now = time.now();
+		retrospectRepository.updateRetrospectStatus(now);
+		log.info("Batch : updateRetrospectStatusToDone");
+	}
+}
