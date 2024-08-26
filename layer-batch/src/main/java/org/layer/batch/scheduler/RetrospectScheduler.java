fcd8738b9e51eb5e4ee712105a51e3bb51b28d64
mikekks <thdalsrb79@naver.com>
2024-08-27 01:21:39 +0900

    chore: 배포 스크립트 수정 후 테스트

--- layer-batch/src/main/java/org/layer/batch/scheduler/RetrospectScheduler.java
+++ layer-batch/src/main/java/org/layer/batch/scheduler/RetrospectScheduler.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class RetrospectScheduler {
+
 	private final RetrospectRepository retrospectRepository;
 	private final Time time;
 
