e07f4ace1e8a7f3cddb642661a8c3950acce58c1
Raymond <akdfhr2@gmail.com>
2024-08-28 10:37:48 +0900

    hotfix: 스페이스 조회 정렬 기준 변경

--- layer-api/src/main/java/org/layer/LayerApplication.java
+++ layer-api/src/main/java/org/layer/LayerApplication.java
@@ -11,8 +11,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @OpenAPIDefinition(servers = {
+        @Server(url = "http://localhost:8080", description = "로컬서버"),
         @Server(url = "https://stgapi.layerapp.io", description = "개발서버"),
-        @Server(url = "https://api.layerapp.io", description = "운영서버")})
+        @Server(url = "https://api.layerapp.io", description = "운영서버")
+})
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAspectJAutoProxy
