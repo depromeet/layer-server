b934058ec1a33427cda685e3efefa08979b70442
Raymond <akdfhr2@gmail.com>
2024-08-30 02:42:33 +0900

    fix: 내가 참여한 회고가 아니더라도 분석 정보 조회 가능

--- layer-api/src/main/java/org/layer/domain/analyze/service/AnalyzeService.java
+++ layer-api/src/main/java/org/layer/domain/analyze/service/AnalyzeService.java
@@ -1,10 +1,7 @@
 package org.layer.domain.analyze.service;
 
-import static org.layer.domain.answer.entity.Answers.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.layer.domain.analyze.controller.dto.response.AnalyzeIndividualGetResponse;
 import org.layer.domain.analyze.controller.dto.response.AnalyzeTeamGetResponse;
 import org.layer.domain.analyze.controller.dto.response.AnalyzesGetResponse;
@@ -21,8 +18,8 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -30,36 +27,36 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AnalyzeService {
 
-	private final AnalyzeRepository analyzeRepository;
-	private final MemberSpaceRelationRepository memberSpaceRelationRepository;
-	private final SpaceRepository spaceRepository;
+    private final AnalyzeRepository analyzeRepository;
+    private final MemberSpaceRelationRepository memberSpaceRelationRepository;
+    private final SpaceRepository spaceRepository;
 
-	private final AIAnalyzeService aiAnalyzeService;
+    private final AIAnalyzeService aiAnalyzeService;
 
-	@Transactional
-	@Async
-	public void createAnalyzeTemp(Long spaceId, Long retrospectId, List<Long> memberIds) {
-		aiAnalyzeService.createAnalyze(spaceId, retrospectId, memberIds);
-	}
+    @Transactional
+    @Async
+    public void createAnalyzeTemp(Long spaceId, Long retrospectId, List<Long> memberIds) {
+        aiAnalyzeService.createAnalyze(spaceId, retrospectId, memberIds);
+    }
 
-	public AnalyzesGetResponse getAnalyze(Long spaceId, Long retrospectId, Long memberId) {
-		// 해당 스페이스 팀원인지 검증
-		Team team = new Team(memberSpaceRelationRepository.findAllBySpaceId(spaceId));
-		team.validateTeamMembership(memberId);
+    public AnalyzesGetResponse getAnalyze(Long spaceId, Long retrospectId, Long memberId) {
+        // 해당 스페이스 팀원인지 검증
+        Team team = new Team(memberSpaceRelationRepository.findAllBySpaceId(spaceId));
+        team.validateTeamMembership(memberId);
 
-		Space space = spaceRepository.findByIdOrThrow(spaceId);
+        Space space = spaceRepository.findByIdOrThrow(spaceId);
 
-		AnalyzeTeamGetResponse analyzeTeamGetResponse = null;
-		if(space.getCategory().equals(SpaceCategory.TEAM)) {
-			Analyze teamAnalyze = analyzeRepository.findByRetrospectIdAndAnalyzeTypeOrThrow(retrospectId,
-				AnalyzeType.TEAM);
-			analyzeTeamGetResponse = AnalyzeTeamGetResponse.of(teamAnalyze);
-		}
+        AnalyzeTeamGetResponse analyzeTeamGetResponse = null;
+        if (space.getCategory().equals(SpaceCategory.TEAM)) {
+            Analyze teamAnalyze = analyzeRepository.findByRetrospectIdAndAnalyzeTypeOrThrow(retrospectId,
+                    AnalyzeType.TEAM);
+            analyzeTeamGetResponse = AnalyzeTeamGetResponse.of(teamAnalyze);
+        }
 
-		Analyze individualAnalyze = analyzeRepository.findByRetrospectIdAndAnalyzeTypeAndMemberIdOrThrow(retrospectId,
-			AnalyzeType.INDIVIDUAL, memberId);
+        Optional<Analyze> individualAnalyze = analyzeRepository.findByRetrospectIdAndAnalyzeTypeAndMemberId(retrospectId,
+                AnalyzeType.INDIVIDUAL, memberId);
 
-		return AnalyzesGetResponse.of(analyzeTeamGetResponse, AnalyzeIndividualGetResponse.of(individualAnalyze));
-	}
+        return AnalyzesGetResponse.of(analyzeTeamGetResponse, individualAnalyze.map(AnalyzeIndividualGetResponse::of).orElse(null));
+    }
 
 }
