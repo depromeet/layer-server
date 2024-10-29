c2c187745a6dfdb55df76fa850cc6cd3db3e4c42
Sejeong Kim <clearrworld@gmail.com>
2024-10-29 22:25:34 +0900

    chore: prod > blue,green 추가

--- layer-api/infra/production/deploy.sh
+++ layer-api/infra/production/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IS_GREEN=$(docker ps | grep green) # 현재 실행중인 App이 blue인지 확인합니다.
+IS_GREEN=$(sudo docker ps | grep green) # 현재 실행중인 App이 blue인지 확인합니다.
 DEFAULT_CONF=" /etc/nginx/nginx.conf"
 
 if [ -z $IS_GREEN  ];then # blue라면
@@ -9,16 +9,16 @@ if [ -z $IS_GREEN  ];then # blue라면
 
   echo "1. get green image"
   cd ./layer-api/infra/production
-  docker-compose pull clean01/layer-server_layer-api:latest # green으로 이미지를 내려받습니다.
+  sudo docker-compose pull clean01/layer-server_layer-api:latest # green으로 이미지를 내려받습니다.
 
   echo "2. green container up"
-  docker-compose up -d green # green 컨테이너 실행
+  sudo docker-compose up -d green # green 컨테이너 실행
 
   while [ 1 = 1 ]; do
   echo "3. green health check..."
-  sleep 3
+  sudo sleep 3
 
-  REQUEST=$(curl http://127.0.0.1:8080) # green으로 request
+  REQUEST=$(sudo curl http://127.0.0.1:8080) # green으로 request
     if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
             echo "health check success"
             break ;
@@ -30,14 +30,14 @@ if [ -z $IS_GREEN  ];then # blue라면
   sudo nginx -s rel
 
   echo "5. blue container down"
-  docker-compose stop blue
+  sudo docker-compose stop blue
 else
   echo "1. get green image"
   cd ./layer-api/infra/production
-  docker-compose pull clean01/layer-server_layer-api:latest # green으로 이미지를 내려받습니다.
+  sudo docker-compose pull clean01/layer-server_layer-api:latest # green으로 이미지를 내려받습니다.
 
   echo "2. blue container up"
-  docker-compose up -d blue # green 컨테이너 실행
+  sudo docker-compose up -d blue # green 컨테이너 실행
 
 
   while [ 1 = 1 ]; do
@@ -56,5 +56,5 @@ else
   sudo nginx -s reload
 
   echo "5. green container down"
-  docker-compose stop green
+  sudo docker-compose stop green
 fi
