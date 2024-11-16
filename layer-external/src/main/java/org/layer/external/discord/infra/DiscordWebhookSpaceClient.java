4ee049fd08f25c550781e8952ceb9ff373bd8731
mikekks <thdalsrb79@naver.com>
2024-11-17 00:30:36 +0900

    feat: 스페이스 생성 시 디스코드 알림 기능 구현

---
+++ layer-external/src/main/java/org/layer/external/discord/infra/DiscordWebhookSpaceClient.java
@@ -0,0 +1,14 @@
+package org.layer.external.discord.infra;
+
+import java.util.Map;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "DiscordWebhookSpaceClient", url = "${discord.webhook.space-url}")
+public interface DiscordWebhookSpaceClient {
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	void sendNotification(@RequestBody Map<String, Object> body);
+}
