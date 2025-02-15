4ab2bf607028a14e380404ae1244297fdc751bc9
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2025-02-15 17:48:43 +0900

    refactor: aws t2.micro로 변경을 위해 admin, batch 소스를 layer-api로 이관 (#289)
    * refactor: layer-admin 소스를 layer-api로 이관
    
    * refactor: layer-batch 소스를 layer-api로 이관
    
    * refactor: layer-api > infra, .github에서 batch, admin 관련 코드 제거
    
    * refactor: layer-api > infra > develop, nginx 설정 파일에서 admin, batch와 관련한 부분 삭제
    
    * chore: dev환경 배포 테스트
    
    * chore: dev환경 배포 테스트2
    
    * chore: dev환경 배포 테스트3
    
    * fix: --remove-orphans 옵션 추가
    
    * refactor: prod환경 배포 테스트
    
    * refactor: prod환경 배포 테스트2(--remove-orphan 옵션 삭제)
    
    * fix: prod에 --remove-orphans 옵션 추가
    
    * fix: dev에서 --remove-orphans 옵션 삭제
    
    * fix: prod에서 --remove-orphans 옵션 삭제

---
+++ layer-api/src/main/java/org/layer/domain/admin/space/controller/AdminSpaceApi.java
@@ -0,0 +1,47 @@
+package org.layer.domain.admin.space.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.layer.domain.admin.retrospect.controller.dto.AdminRetrospectCountGetResponse;
+import org.layer.domain.admin.space.controller.dto.AdminSpaceCountGetResponse;
+import org.layer.domain.admin.space.controller.dto.AdminSpacesGetResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+
+@Tag(name = "[ADMIN] 스페이스 데이터", description = "스페이스 관련 api")
+public interface AdminSpaceApi {
+    @Operation(summary = "스페이스 관련 데이터 조회", description = "")
+    @Parameters({
+            @Parameter(name = "startDate", description = "검색 시작 시간", example = "2024-09-05T15:30:45", required = true),
+            @Parameter(name = "endDate", description = "검색 종료 시간", example = "2024-09-13T15:30:45", required = true),
+            @Parameter(name = "password", description = "비밀번호 [카톡방으로 공유]", example = "[카톡방으로 공유]", required = true)
+    })
+    ResponseEntity<AdminSpacesGetResponse> getSpaceData(@RequestParam("startDate") LocalDateTime startDate,
+                                                        @RequestParam("endDate") LocalDateTime endDate, @RequestParam("password") String password);
+
+    @Operation(summary = "스페이스 개수 조회", description = "특정 기간내에 만들어진 스페이스 개수를 조회합니다. (우리 팀원이 만든 스페이스는 제외)")
+    @Parameters({
+            @Parameter(name = "startDate", description = "검색 시작 시간", example = "2024-09-05T15:30:45", required = true, schema = @Schema(type = "string")),
+            @Parameter(name = "endDate", description = "검색 종료 시간", example = "2024-09-13T15:30:45", required = true, schema = @Schema(type = "string")),
+            @Parameter(name = "password", description = "비밀번호 [카톡방으로 공유]", example = "[카톡방으로 공유]", required = true, schema = @Schema(type = "string", format = "string"))})
+    ResponseEntity<AdminSpaceCountGetResponse> getSpaceCount(@RequestParam("startDate") LocalDateTime startDate,
+                                                             @RequestParam("endDate") LocalDateTime endDate, @RequestParam("password") String password);
+
+
+    @Operation(summary = "특정 스페이스 내 회고 개수 조회", description = "특정 기간내에 특정 스페이스 안에서 시작된 회고 개수를 조회합니다.")
+    @Parameters({
+            @Parameter(name = "startDate", description = "검색 시작 시간", example = "2024-09-05T15:30:45", required = true, schema = @Schema(type = "string")),
+            @Parameter(name = "endDate", description = "검색 종료 시간", example = "2024-09-13T15:30:45", required = true, schema = @Schema(type = "string")),
+            @Parameter(name = "password", description = "비밀번호 [카톡방으로 공유]", example = "[카톡방으로 공유]", required = true, schema = @Schema(type = "string", format = "string"))})
+    ResponseEntity<AdminRetrospectCountGetResponse> getRetrospectCountInSpace(@RequestParam("startDate") LocalDateTime startDate,
+                                                                              @RequestParam("endDate") LocalDateTime endDate,
+                                                                              @PathVariable("spaceId") Long spaceId,
+                                                                              @RequestParam("password") String password);
+
+}
---
+++ layer-api/src/main/java/org/layer/domain/admin/space/controller/dto/AdminSpacesGetResponse.java
@@ -0,0 +1,21 @@
+package org.layer.domain.admin.space.controller.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.layer.domain.space.dto.AdminSpaceGetResponse;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+@Schema(name = "AdminSpacesGetResponse", description = "Admin 스페이스 조회")
+public class AdminSpacesGetResponse {
+
+	@Schema(description = "스페이스 객체", example = "")
+	private final List<AdminSpaceGetResponse> spaces;
+
+	@Schema(description = "총 개수", example = "30")
+	private final Integer totalCount;
+
+}
