875e08573039a77cc5c8e60332e1a9700b45e4b2
mikekks <thdalsrb79@naver.com>
2025-04-21 22:37:04 +0900

    test: 기존 테스트 코드 에러 해결

--- layer-api/src/test/java/org/layer/domain/retrospect/service/RetrospectServiceTest.java
+++ layer-api/src/test/java/org/layer/domain/retrospect/service/RetrospectServiceTest.java
@@ -9,9 +9,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.layer.domain.fixture.MemberFixture;
+import org.layer.domain.fixture.SpaceFixture;
 import org.layer.domain.member.entity.Member;
-import org.layer.domain.member.entity.MemberRole;
-import org.layer.domain.member.entity.SocialType;
 import org.layer.domain.member.repository.MemberRepository;
 import org.layer.domain.question.enums.QuestionType;
 import org.layer.domain.retrospect.controller.dto.request.QuestionCreateRequest;
@@ -21,7 +21,6 @@ import org.layer.domain.retrospect.entity.RetrospectStatus;
 import org.layer.domain.retrospect.repository.RetrospectRepository;
 import org.layer.domain.space.entity.MemberSpaceRelation;
 import org.layer.domain.space.entity.Space;
-import org.layer.domain.space.entity.SpaceCategory;
 import org.layer.domain.space.repository.MemberSpaceRelationRepository;
 import org.layer.domain.space.repository.SpaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,19 +54,12 @@ class RetrospectServiceTest {
 		@DisplayName("정상 입력의 경우 회고 생성에 성공한다.")
 		void createRetrospectTest_1() {
 			// given
-			Member member = Member.builder()
-				.name("홍길동")
-				.email("qwer@naver.com")
-				.memberRole(MemberRole.USER)
-				.socialType(SocialType.GOOGLE)
-				.socialId("123456789")
-				.build();
-			Member member1 = memberRepository.saveAndFlush(member);
+			Member member = MemberFixture.createFixture("social-1");
+			member = memberRepository.save(member);
 
-			Space space = new Space("banner-url", SpaceCategory.TEAM, List.of(DEVELOPMENT), "회고스페이스 이름", "회고스페이스 설명",
-				1L, 1L);
-			spaceRepository.saveAndFlush(space);
-			memberSpaceRelationRepository.saveAndFlush(new MemberSpaceRelation(member1.getId(), space));
+			Space space = SpaceFixture.createFixture(member.getId(), 1L);
+			spaceRepository.save(space);
+			memberSpaceRelationRepository.save(new MemberSpaceRelation(member.getId(), space));
 
 			RetrospectCreateRequest request = new RetrospectCreateRequest("회고제목입니다", "회고소개입니다",
 				List.of(new QuestionCreateRequest("질문1", QuestionType.PLAIN_TEXT.getStyle()),
@@ -77,7 +69,7 @@ class RetrospectServiceTest {
 				false, null, null, null, false);
 
 			// when
-			Long savedId = retrospectService.createRetrospect(request, 1L, 1L);
+			Long savedId = retrospectService.createRetrospect(request, space.getId(), member.getId());
 
 			// then
 			Retrospect retrospect = retrospectRepository.findByIdOrThrow(savedId);
