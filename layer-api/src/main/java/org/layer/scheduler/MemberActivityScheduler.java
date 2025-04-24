1a98f52bd9bbd51d0a0bb64804a06efd8b8abe40
mikekks <thdalsrb79@naver.com>
2025-04-24 18:12:12 +0900

    feat: 실패상황 고려하여 redis 저장로직 추가

--- layer-api/src/main/java/org/layer/scheduler/MemberActivityScheduler.java
+++ layer-api/src/main/java/org/layer/scheduler/MemberActivityScheduler.java
@@ -1,8 +1,11 @@
 package org.layer.scheduler;
 
+import java.time.LocalDateTime;
+
 import org.layer.admin.cache.MemberActivityCache;
 import org.layer.discord.event.MemberActivityEvent;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +20,15 @@ public class MemberActivityScheduler {
 	private final MemberActivityCache memberActivityCache;
 	private final ApplicationEventPublisher eventPublisher;
 
+	private final RedisTemplate<String, String> redisTemplate;
+
 	// 매일 오후 9시에 실행됨
 	@Scheduled(cron = "0 0 21 * * *")
 	public void logDailyMemberActivity() {
 		log.info("🌙 유저 활동 통계 작업 시작");
 
 		eventPublisher.publishEvent(new MemberActivityEvent(memberActivityCache.activityMap));
+		redisTemplate.opsForValue().set(LocalDateTime.now().toString(), memberActivityCache.getMessage());
 		memberActivityCache.activityMap.clear();
 	}
 }
