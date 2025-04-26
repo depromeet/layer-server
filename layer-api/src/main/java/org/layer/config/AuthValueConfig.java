5531612cb93ee2e6a82a3b3b1e8683e113fb20b4
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2025-04-26 21:05:28 +0900

    chore: 미사용 코드 제거 (#335)

--- layer-api/src/main/java/org/layer/config/AuthValueConfig.java
+++ layer-api/src/main/java/org/layer/config/AuthValueConfig.java
@@ -14,12 +14,8 @@ public class AuthValueConfig {
     @Value("${jwt.secret}")
     private String JWT_SECRET;
 
-    public static final Long ACCESS_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 30L; // 한달
-    //    public static final Long ACCESS_TOKEN_EXPIRATION_TIME = 1000 * 60 * 30L; // 30분
+    public static final Long ACCESS_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60L; // 1시간
     public static final Long REFRESH_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 14L; // 2주
-    public static final String AUTHORIZATION = "Authorization";
-    public static final String KAKAO_URI = "https://kapi.kakao.com/v2/user/me";
-
 
     @PostConstruct
     protected void init() {
