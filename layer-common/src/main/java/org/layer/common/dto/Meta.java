56a547dce9756ddacc0492b57d9be987b72a9f85
mikekks <thdalsrb79@naver.com>
2025-06-18 14:02:06 +0900

    feat: layer-common 모듈 의존성 정리

--- layer-common/src/main/java/org/layer/common/dto/Meta.java
+++ layer-common/src/main/java/org/layer/common/dto/Meta.java
@@ -1,6 +1,5 @@
 package org.layer.common.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
@@ -8,10 +7,9 @@ import lombok.Data;
 @Builder
 @Data
 public class Meta {
-    @Schema()
+
     @NotNull
     private boolean hasNextPage;
 
-    @Schema(nullable = true)
     private Long cursor;
 }
