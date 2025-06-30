99caacc5c1ba16deeb0ed1fb7a6c6a9ba6bd6319
mikekks <thdalsrb79@naver.com>
2025-06-30 17:14:56 +0900

    feat: admin 관련 인가 인터셉터 구현

---
+++ layer-admin/src/main/java/org/layer/admin/auth/AdminAuthorizationInterceptor.java
@@ -0,0 +1,28 @@
+package org.layer.admin.auth;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class AdminAuthorizationInterceptor implements HandlerInterceptor {
+
+	@Value("${admin.password}")
+	private String adminPassword;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		String headerValue = request.getHeader("X-ADMIN-TOKEN");
+
+		if (headerValue == null || !headerValue.equals(adminPassword)) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.getWriter().write("Unauthorized");
+			return false;
+		}
+
+		return true; // 통과
+	}
+}
