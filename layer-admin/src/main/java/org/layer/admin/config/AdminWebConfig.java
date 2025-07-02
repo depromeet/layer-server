50b131fbabe4a01971164bb7129a5cbf4741d042
mg <100754581+mikekks@users.noreply.github.com>
2025-07-02 14:14:34 +0900

    [admin] feat: 인가 인터셉터 구현 (#358)
    * feat: admin 관련 인가 인터셉터 구현
    
    * rename: admin prefix 추가

---
+++ layer-admin/src/main/java/org/layer/admin/config/AdminWebConfig.java
@@ -0,0 +1,22 @@
+package org.layer.admin.config;
+
+import org.layer.admin.auth.AdminAuthorizationInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AdminWebConfig implements WebMvcConfigurer {
+
+	private final AdminAuthorizationInterceptor adminInterceptor;
+
+	public AdminWebConfig(AdminAuthorizationInterceptor adminInterceptor) {
+		this.adminInterceptor = adminInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(adminInterceptor)
+			.addPathPatterns("/admin/**"); // /admin 으로 시작하는 모든 경로에 적용
+	}
+}
