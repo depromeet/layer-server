175a41a3fc393ae8a956b9e9608f0d00ed601ee0
mikekks <thdalsrb79@naver.com>
2025-07-02 15:07:36 +0900

    feat: 로그인 기능 구현

---
+++ layer-admin/src/main/java/org/layer/admin/auth/controller/AdminAuthController.java
@@ -0,0 +1,18 @@
+package org.layer.admin.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class AdminAuthController {
+
+	@RequestMapping("/admin/login")
+	public ResponseEntity<String> login() {
+		// 인터셉터 단에서 인증을 처리하므로, 여기서는 단순히 성공 메시지를 반환합니다.
+		return ResponseEntity.ok("Admin login successful");
+	}
+}
