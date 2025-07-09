103c925715183177e6148c816cd712957a27a9b5
mikekks <thdalsrb79@naver.com>
2025-07-09 17:04:54 +0900

    chore: 컨테이너 이름 dev, prod 에 맞게 수정

--- layer-api/infra/production/deploy.sh
+++ layer-api/infra/production/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IS_GREEN=$(sudo docker ps | grep layer-api-green) # 현재 실행중인 App이 blue인지 확인합니다.
+IS_GREEN=$(sudo docker ps | grep layer-api-prod-green) # 현재 실행중인 App이 blue인지 확인합니다.
 
 if [ -z $IS_GREEN  ];then # blue라면
 
