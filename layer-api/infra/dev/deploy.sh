e754a3c0307299fc1208db30c70061ccf5cb0ba2
mg <100754581+mikekks@users.noreply.github.com>
2025-07-11 15:09:48 +0900

    infra: 배포 프로세스 개선 및 dockerhub 계정이관 (#377)
    * rename: 디렉토리 이름 수정
    
    * refactor: dockerhub 계정이관 및 불필요 단계 간소화 및 개선
    
    * refactor: 배포스크립트 재사용성 향상

---
+++ layer-api/infra/dev/deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+IS_GREEN=$(sudo docker ps --format '{{.Ports}}' | grep 8090) # 현재 실행중인 App이 blue인지 확인합니다.
+
+PORT_NUMBER=0
+NEXT_DOCKER_COMPOSE_FILE=""
+PREV_DOCKER_COMPOSE_FILE=""
+
+if [ -z "$IS_GREEN"  ];then # green 실행되어야 함(현재 blue 실행중)
+  PORT_NUMBER=8090
+  NEXT_DOCKER_COMPOSE_FILE="docker-compose-green.yaml"
+  PREV_DOCKER_COMPOSE_FILE="docker-compose-blue.yaml"
+  DOWN_CONTAINER_NAME="layer-api-dev-blue"
+else # blue 실행되어야 함(현재 green 실행중)
+  PORT_NUMBER=8091
+  NEXT_DOCKER_COMPOSE_FILE="docker-compose-blue.yaml"
+  PREV_DOCKER_COMPOSE_FILE="docker-compose-green.yaml"
+  DOWN_CONTAINER_NAME="layer-api-dev-green"
+fi
+
+
+# 여기서부터 배포 스크립트 시작
+echo "1. pull latest image"
+sudo docker compose -f $NEXT_DOCKER_COMPOSE_FILE pull
+
+echo "2. container up"
+sudo docker compose -f $NEXT_DOCKER_COMPOSE_FILE up -d
+
+while true; do
+  echo "3. health check..."
+  sleep 3
+  REQUEST=$(curl -s http://127.0.0.1:$PORT_NUMBER/actuator/health)
+  if echo "$REQUEST" | grep -q '"status":"UP"'; then
+    echo "✅ health check success"
+    break
+  fi
+done
+
+echo "4. reload nginx config"
+export DEV_PORT=$PORT_NUMBER
+sudo envsubst '${DEV_PORT}' < /home/ubuntu/layer-api/infra/nginx.template.conf > /etc/nginx/nginx.conf
+sudo nginx -s reload
+
+echo "5. remove old container"
+sudo docker compose -f $PREV_DOCKER_COMPOSE_FILE rm -s -f $DOWN_CONTAINER_NAME
