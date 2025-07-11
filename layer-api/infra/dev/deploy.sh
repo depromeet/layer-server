40bd691181b36fcd24301c3cbb23c49acb5658f5
mikekks <thdalsrb79@naver.com>
2025-07-11 15:03:56 +0900

    refactor: 배포스크립트 재사용성 향상

--- layer-api/infra/dev/deploy.sh
+++ layer-api/infra/dev/deploy.sh
@@ -1,57 +1,45 @@
 #!/bin/bash
 
-IS_GREEN=$(sudo docker ps | grep layer-api-dev-green) # 현재 실행중인 App이 blue인지 확인합니다.
+IS_GREEN=$(sudo docker ps --format '{{.Ports}}' | grep 8090) # 현재 실행중인 App이 blue인지 확인합니다.
 
-if [ -z $IS_GREEN  ];then # blue라면
+PORT_NUMBER=0
+NEXT_DOCKER_COMPOSE_FILE=""
+PREV_DOCKER_COMPOSE_FILE=""
 
-  echo "### BLUE => GREEN ###"
-  echo "1. pull latest green image"
-  sudo docker compose -f docker-compose-green.yaml pull
-
-  echo "2. green container up"
-  sudo docker compose -f docker-compose-green.yaml up -d
-
-  while [ 1 = 1 ]; do
-  echo "3. green health check..."
-  sudo sleep 3
-
-  REQUEST=$(sudo curl http://127.0.0.1:8090/actuator/health) # green으로 request
-    if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
-            echo "health check success"
-            break ;
-            fi
-  done;
-
-  echo "4. reload nginx"
-  sudo cp ../nginx.green.conf /etc/nginx/nginx.conf
-  sudo nginx -s reload
-
-  echo "5. blue container down"
-  sudo docker compose -f docker-compose-blue.yaml rm -s -f layer-api-dev-blue
-else
-  echo "### GREEN => BLUE ###"
-  echo "1. pull latest blue image"
-  sudo docker compose -f docker-compose-blue.yaml pull
-
-  echo "2. blue container up"
-  sudo docker compose -f docker-compose-blue.yaml up -d
-
-
-  while [ 1 = 1 ]; do
-    echo "3. blue health check..."
-    sleep 3
-    REQUEST=$(curl http://127.0.0.1:8091/actuator/health) # blue로 request
-
-    if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
-      echo "health check success"
-      break ;
-    fi
-  done;
-
-  echo "4. reload nginx"
-  sudo cp ../nginx.blue.conf /etc/nginx/nginx.conf
-  sudo nginx -s reload
-
-  echo "5. green container down"
-  sudo docker compose -f docker-compose-green.yaml rm -s -f layer-api-dev-green
+if [ -z "$IS_GREEN"  ];then # green 실행되어야 함(현재 blue 실행중)
+  PORT_NUMBER=8090
+  NEXT_DOCKER_COMPOSE_FILE="docker-compose-green.yaml"
+  PREV_DOCKER_COMPOSE_FILE="docker-compose-blue.yaml"
+  DOWN_CONTAINER_NAME="layer-api-dev-blue"
+else # blue 실행되어야 함(현재 green 실행중)
+  PORT_NUMBER=8091
+  NEXT_DOCKER_COMPOSE_FILE="docker-compose-blue.yaml"
+  PREV_DOCKER_COMPOSE_FILE="docker-compose-green.yaml"
+  DOWN_CONTAINER_NAME="layer-api-dev-green"
 fi
+
+
+# 여기서부터 배포 스크립트 시작
+echo "1. pull latest image"
+sudo docker compose -f $NEXT_DOCKER_COMPOSE_FILE pull
+
+echo "2. container up"
+sudo docker compose -f $NEXT_DOCKER_COMPOSE_FILE up -d
+
+while true; do
+  echo "3. health check..."
+  sleep 3
+  REQUEST=$(curl -s http://127.0.0.1:$PORT_NUMBER/actuator/health)
+  if echo "$REQUEST" | grep -q '"status":"UP"'; then
+    echo "✅ health check success"
+    break
+  fi
+done
+
+echo "4. reload nginx config"
+export DEV_PORT=$PORT_NUMBER
+sudo envsubst '${DEV_PORT}' < /home/ubuntu/layer-api/infra/nginx.template.conf > /etc/nginx/nginx.conf
+sudo nginx -s reload
+
+echo "5. remove old container"
+sudo docker compose -f $PREV_DOCKER_COMPOSE_FILE rm -s -f $DOWN_CONTAINER_NAME
