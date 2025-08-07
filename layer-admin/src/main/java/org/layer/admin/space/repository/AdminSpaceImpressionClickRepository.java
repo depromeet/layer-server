bebaba5d9e28dd9c27f7482a66f1c1f6646cbc99
mikekks <thdalsrb79@naver.com>
2025-08-07 14:35:06 +0900

    chore: 이름 변경으로 인한 삭제

--- layer-admin/src/main/java/org/layer/admin/space/repository/AdminSpaceImpressionClickRepository.java
+++
@@ -1,7 +0,0 @@
-package org.layer.admin.space.repository;
-
-import org.layer.admin.space.entity.AdminSpaceImpressionClick;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminSpaceImpressionClickRepository extends JpaRepository<AdminSpaceImpressionClick, Long> {
-}
