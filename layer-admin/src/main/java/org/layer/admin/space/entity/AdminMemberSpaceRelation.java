76d09a4a92011f17c13fef32be6998dbfbb0644f
mikekks <thdalsrb79@naver.com>
2025-08-15 15:37:43 +0900

    chore: 기존에 있는 테이블 조회를 위한 수정

--- layer-admin/src/main/java/org/layer/admin/space/entity/AdminMemberSpaceRelation.java
+++ layer-admin/src/main/java/org/layer/admin/space/entity/AdminMemberSpaceRelation.java
@@ -2,29 +2,43 @@ package org.layer.admin.space.entity;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.Subselect;
+import org.hibernate.annotations.Synchronize;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 @Entity
-@Table(name = "member_space_relation")
+@Immutable
+@Subselect("""
+    SELECT id, created_at, updated_at, member_id, space_id
+    FROM member_space_relation
+""")
+@Synchronize("member_space_relation")
 public class AdminMemberSpaceRelation {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(name = "created_at")
 	private LocalDateTime createdAt;
 
+	@Column(name = "updated_at")
 	private LocalDateTime updatedAt;
 
 	@NotNull
+	@Column(name = "member_id")
 	private Long memberId;
 
+	@NotNull
+	@Column(name = "space_id")
 	private Long spaceId;
 }
